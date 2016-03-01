7f93207ad72b44c28319dd21d58b7b652866a0a4
jessesomething <jessespalding@gmail.com>
2016-02-29 21:39:59 -0600

    Added time, date, and tsunami columns

--- Main.py
+++ Main.py
@@ -5,9 +5,12 @@ import sqlite3
 import tkinter as tk
 from tkinter import *
 from tkinter.ttk import *
-import decimal
+import time
+from datetime import datetime
+import webbrowser
 
 eventList = []
+
 
 # url = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson"
 # r = requests.get(url)
@@ -43,13 +46,13 @@ eventList = []
 #     print(events['location'] + "\nLat: " + events['latitude'] + "\nLong: " +
 #           events['longitude'] + "\nMagnitude: " + events['magnitude'] + "\n")
 
-class Events():
-    def __init__(self, name, location, lat, long, tsunami, ):
-        self.name = name
+# class Events():
+#     def __init__(self, name, location, lat, long, tsunami, ):
+#         self.name = name
+
 
 class MainGUI():
     def __init__(self):
-
         # self._set_main().CreateTable()
         # self._set_main().LoadTable()
         # self.grid(sticky = (N,S,W,E))
@@ -68,7 +71,6 @@ class MainGUI():
         self.main_quakes_week = tk.Frame(self.root)
         self._show_quakes_week()
 
-
     def _set_side(self):
         sidebar = tk.Frame(
             self.root, width=self.side_window_width, bg=self.side_bg_color,
@@ -83,6 +85,8 @@ class MainGUI():
         lastWeek.pack()
         lastWeek.place(bordermode='outside', height=50, width=self.sidebar_width, relx=.03, rely=.12)
 
+
+
         exit = tk.Button(self.root, text="Quit", command=quit)
         exit.pack()
         exit.place(bordermode='outside', height=50, width=self.sidebar_width, relx=.03, rely=.333)
@@ -93,28 +97,53 @@ class MainGUI():
         self.root.update()
         self.root.minsize(self.main_window_width, self.window_height)
 
+        get_link = tk.Button(self.root, text="Get link")
+        get_link.pack()
+        get_link.place(bordermode='outside', height=50, width=self.sidebar_width, relx=.03, rely=.22)
+
         self.main_quakes_day = tk.Frame(self.root, bg=self.main_bg_color,
-                                            width=self.main_window_width, height=self.window_height)
+                                        width=self.main_window_width, height=self.window_height)
         self.main_quakes_day.pack(expand=True, fill='both', side='right')
 
         # root = Tk()
         QuakeFrame.CreateUI(self.main_quakes_day)
         url = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson"
+        # url = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"
         r = requests.get(url)
         data = r.text
         quakes = json.loads(data)
 
         for q in quakes['features']:
             print(q)
+            url = q['properties']['url']
             loc = q['properties']['place']
             mag = q['properties']['mag']
             coord = q['geometry']['coordinates']
+            tsunami = q['properties']['tsunami']
+            time_epoch = q['properties']['time']
+            time_stamp = time_epoch / 1000.0
+            time_readable = datetime.fromtimestamp(time_stamp).strftime('%H:%M:%S')
+            date_readable = datetime.fromtimestamp(time_stamp).strftime('%d-%m-%Y')
 
-            QuakeFrame.LoadTable(self.main_quakes_day,loc=loc,mag=mag,coord=coord)
+            print(time_readable)
+            if tsunami == 0:
+                tsunami = 'No'
+            elif tsunami == 1:
+                tsunami = 'Yes'
+
+            QuakeFrame.LoadTable(self.main_quakes_day, loc=loc, link=url, mag=mag, coord=coord, tsunami=tsunami,
+                                 time_utc=time_readable, date_utc=date_readable)
             # print(q['properties']['mag'])
 
-        self.root.mainloop()
 
+
+        def print_something():
+            print('something')
+
+        print_something()
+
+
+        self.root.mainloop()
 
         # tree.insert('', 'end', 'widgets', text='Widget Tour')
         # tree['columns'] = ('size', 'modified', 'owner')
@@ -128,7 +157,7 @@ class MainGUI():
         self.root.update()
         self.root.minsize(self.main_window_width, self.window_height)
         self.main_quakes_week = tk.Frame(self.root, bg=self.main_bg_color,
-                                            width=self.main_window_width, height=self.window_height)
+                                         width=self.main_window_width, height=self.window_height)
         self.main_quakes_week.pack(expand=True, fill='both', side='right')
         QuakeFrame.CreateUI(self.main_quakes_day)
         # QuakeFrame.LoadTable(self.main_quakes_day)
@@ -138,7 +167,8 @@ class MainGUI():
 class QuakeFrame(Frame):
     def __init__(self, parent):
         Frame.__init__(parent)
-        url = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson"
+        # url = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson"
+        # url = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"
         # r = requests.get(url)
         # data = r.text
         # quakes = json.loads(data)
@@ -147,37 +177,50 @@ class QuakeFrame(Frame):
         #     print(q['properties']['mag'])
         self.CreateUI()
         # self.LoadTable(loc=0,mag=0,coord=0)
-        self.grid(sticky = (N,S,W,E))
-        parent.grid_rowconfigure(0, weight = 1)
-        parent.grid_columnconfigure(0, weight = 1)
+        self.grid(sticky=(N, S, W, E))
+        parent.grid_rowconfigure(0, weight=1)
+        parent.grid_columnconfigure(0, weight=1)
 
     def CreateUI(self):
         tv = Treeview(self)
-        tv['columns'] = ('location', 'magnitude', 'coordinates')
+        tv['columns'] = ('location', 'link', 'magnitude', 'coordinates', 'tsunami', 'time')
         tv.heading('#0', text='Location', anchor='w')
-        tv.column('location', anchor='center', width=200)
-        tv.heading('#1', text='Magnitude')
-        tv.column('magnitude', anchor='center', width=50)
-        tv.heading('#2', text='Coordinates')
-        tv.column('coordinates', anchor='center', width=100)
-        tv.grid(sticky = (N,S,W,E))
+        tv.column('location', anchor='center', width=100)
+        tv.heading('#1', text='Link')
+        tv.column('link', anchor='center', width=75)
+        tv.heading('#2', text='Magnitude')
+        tv.column('magnitude', anchor='center', width=150)
+        tv.heading('#3', text='Coordinates')
+        tv.column('coordinates', anchor='center', width=55)
+        tv.heading('#4', text='Tsunami')
+        tv.column('tsunami', anchor='center', width=50)
+        tv.heading('#5', text='Time')
+        tv.column('time', anchor='center', width=100)
+        tv.heading('#6', text='Date')
+        tv.grid(sticky=(N, S, W, E))
         self.treeview = tv
-        self.grid_rowconfigure(0, weight = 1)
-        self.grid_columnconfigure(0, weight = 1)
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
 
-    def LoadTable(self, loc, mag, coord):
-        self.treeview.insert('', 'end', text=loc, values=(mag, coord))
+    def LoadTable(self, loc, link, mag, coord, tsunami, time_utc, date_utc):
+        self.treeview.insert('', 'end', text=loc, values=(link, mag, coord, tsunami, time_utc, date_utc))
+    #
+    # def get_focus(self):
+    #     self.treeview.focus_get()
 
 
 
 def start_gui():
     MainGUI().mainloop()
 
+
 def quit():
     exit()
 
+
 def main():
     start_gui()
+
 
 if __name__ == '__main__':
     main()
